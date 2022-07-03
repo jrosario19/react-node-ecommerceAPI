ebfe108333fa3b8e4588a075f2484df343ffd041
Juan Rosario <jrosario19@hotmail.com>
2022-07-03 11:27:44 -0400

    Update index

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
+const cors=require("cors")
 
 
 dotenv.config()
@@ -14,8 +20,15 @@ mongoose.connect(
     console.log(err)
 })
 
+
+app.use(cors());
 app.use(express.json());
-app.use("/api/users", userRoute )
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/products", productRoute)
+app.use("/api/carts", cartRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/checkout", stripeRoute)
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running")
