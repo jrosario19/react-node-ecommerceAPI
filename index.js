f9f172c53f17ab76767e8b6f9fb143b0742fb3c6
Juan Rosario <jrosario19@hotmail.com>
2022-10-29 12:46:23 -0400

    Update index

--- index.js
+++ index.js
@@ -19,9 +19,13 @@ mongoose.connect(
 ).then(()=>console.log("DB connection Successfull!")).catch((err)=>{
     console.log(err)
 })
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
-
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
